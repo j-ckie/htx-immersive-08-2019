169f58c730e8c01ed26a074c818d37f0d18149bf
Jaclyn Legacion <jglegacion@gmail.com>
2019-09-03 11:15:02 -0500

    Add array of dictionaries example

---
+++ 02-week/1-tuesday/lectures/dictionary-lecture-example.py
@@ -0,0 +1,8 @@
+# Array of Dictionaries
+people = [
+    {"name" : "John", "age" : 55},
+    {"name" : "Micah", "age" : 40}
+]
+
+# Get Micah
+print(people[1]["name"]) # reference index 1, key "name"
